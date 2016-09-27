68e135513c69ef97a8f1e9ec8fa72b96ceceebe9
dd <sinfere@gmail.com>
2016-09-27 13:56:34 +0800

    add scheduled task example

--- src/main/java/com/dix/BootApplication.java
+++ src/main/java/com/dix/BootApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan
+@EnableScheduling
 public class BootApplication {
 
     public static void main(String[] args) throws Exception {
