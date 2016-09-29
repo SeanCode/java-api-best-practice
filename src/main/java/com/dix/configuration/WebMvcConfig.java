bdb1b8ade20e77b692a0bf3fda40d522ec9ab4bc
dd <sinfere@gmail.com>
2016-09-29 18:06:46 +0800

    add cors

--- src/main/java/com/dix/configuration/WebMvcConfig.java
+++ src/main/java/com/dix/configuration/WebMvcConfig.java
@@ -2,7 +2,12 @@ package com.dix.configuration;
 
 import com.dix.interceptor.SecurityInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,27 +15,37 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-  private final SecurityInterceptor securityInterceptor;
+    private final SecurityInterceptor securityInterceptor;
 
-  @Autowired
-  public WebMvcConfig(SecurityInterceptor securityInterceptor) {
+    @Autowired
+    public WebMvcConfig(SecurityInterceptor securityInterceptor) {
     this.securityInterceptor = securityInterceptor;
   }
 
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-      registry.addInterceptor(securityInterceptor)
-              .addPathPatterns("/**")
-              .excludePathPatterns("/error**")
-              .excludePathPatterns("/static/**")
-      ;
-  }
-
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("OPTIONS", "HEAD", "POST", "GET");
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(securityInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/error**")
+                .excludePathPatterns("/static/**")
+        ;
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("OPTIONS");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 
 }
