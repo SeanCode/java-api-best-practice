a275db54da6d589f9416e86779064675aed5f9e3
dd <sinfere@gmail.com>
2016-10-10 22:39:35 +0800

    file upload related config not required

--- src/main/java/com/dix/base/service/UploadService.java
+++ src/main/java/com/dix/base/service/UploadService.java
@@ -1,6 +1,9 @@
 package com.dix.base.service;
 
 import com.dix.app.controller.FileController;
+import com.dix.base.common.Core;
+import com.dix.base.common.SpringMybatisObjectFactory;
+import com.dix.base.exception.InvalidConfigurationException;
 import com.dix.base.exception.WrongParamException;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -25,16 +29,16 @@ public class UploadService {
 
     private static Logger logger = LoggerFactory.getLogger(UploadService.class);
 
-    @Value("${file.upload.root}")
+    @Value("${file.upload.root:#{null}}")
     private String fileRoot;
 
-    @Value("${file.upload.image.root}")
+    @Value("${file.upload.image.root:#{null}}")
     private String imageRoot;
 
-    @Value("${file.upload.image.mime.type}")
+    @Value("${file.upload.image.mime.type:image/png,image/jpeg,image/jpg,image/gif}")
     private String allowedImageMimeType;
 
-    @Value("${file.upload.image.size.max}")
+    @Value("${file.upload.image.size.max:1048576}")
     private Long maxImageSize;
 
     public void validateFile(MultipartFile file)
@@ -56,12 +60,22 @@ public class UploadService {
     }
 
     public void ensureImageRoot() throws IOException {
+        if (fileRoot == null || imageRoot == null)
+        {
+            throw new InvalidConfigurationException("file.upload.root or file.upload.image.root not set");
+        }
+
         File directory = new File(String.format("%s/%s", fileRoot, imageRoot));
         Files.createDirectories(directory.toPath());
     }
 
     public Path getImageFilePath(String fileName)
     {
+        if (fileRoot == null || imageRoot == null)
+        {
+            throw new InvalidConfigurationException("file.upload.root or file.upload.image.root not set");
+        }
+
         return Paths.get(fileRoot, imageRoot, fileName);
     }
 
