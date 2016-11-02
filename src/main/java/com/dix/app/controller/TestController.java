9ee40ee19d917506d976b0956b4cd50389aa0275
dd <sinfere@gmail.com>
2016-11-02 20:18:50 +0800

    add mailer and freemarker

--- src/main/java/com/dix/app/controller/TestController.java
+++ src/main/java/com/dix/app/controller/TestController.java
@@ -1,21 +1,41 @@
 package com.dix.app.controller;
 
+import com.dix.app.model.User;
 import com.dix.base.common.DataResponse;
 import com.dix.app.service.UserService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
     @Autowired
-    public TestController(@Qualifier("RedisTemplate") RedisTemplate<String, String> template, UserService userService) {
+    public TestController(@Qualifier("RedisTemplate") RedisTemplate<String, String> template, UserService userService, Configuration configuration) {
         this.template = template;
         this.userService = userService;
+        this.configuration = configuration;
     }
 
     @RequestMapping("/transaction")
@@ -24,8 +44,78 @@ public class TestController {
         return DataResponse.create();
     }
 
+    @RequestMapping("/template")
+    public DataResponse template() throws IOException, TemplateException {
+        Template template = configuration.getTemplate("hello.tpl");
+        User user = userService.findById(1L);
+        Map<String, Object> data = new HashMap<>();
+        data.put("user", user);
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, data);
+        return DataResponse.create()
+                .put("content", content)
+                ;
+    }
+
+    @RequestMapping("/email")
+    public DataResponse email() throws IOException, TemplateException, EmailException {
+        Template template = configuration.getTemplate("hello.tpl");
+        User user = userService.findById(1L);
+        Map<String, Object> data = new HashMap<>();
+        data.put("user", user);
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, data);
+
+        Email email = new SimpleEmail();
+        email.setHostName("smtp-mail.outlook.com");
+        // email.setSslSmtpPort("587");
+        email.setSmtpPort(587);
+        email.setSocketTimeout(60 * 1000);
+        email.setSocketConnectionTimeout(10 * 1000);
+        email.setAuthentication("xxx", "xxx");
+        // email.setSSLOnConnect(false);
+        email.setStartTLSEnabled(true);
+        // email.setStartTLSRequired(true);
+        email.setFrom("xxx");
+        email.setSubject("hello");
+        email.setMsg(content);
+        email.addTo("xxx");
+        email.send();
+
+        return DataResponse.create()
+                ;
+    }
+
+    @RequestMapping("/email-qq")
+    public DataResponse emailQQ() throws IOException, TemplateException, EmailException {
+        Template template = configuration.getTemplate("hello.tpl");
+        User user = userService.findById(1L);
+        Map<String, Object> data = new HashMap<>();
+        data.put("user", user);
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, data);
+
+        Email email = new SimpleEmail();
+        email.setHostName("smtp.qq.com");
+        email.setSslSmtpPort("465");
+        email.setSmtpPort(465);
+        email.setSocketTimeout(30 * 1000);
+        email.setSocketConnectionTimeout(10 * 1000);
+        email.setAuthentication("xxx", "xxx");
+        email.setSSLOnConnect(true);
+        email.setStartTLSEnabled(true);
+        // email.setStartTLSRequired(true);
+        email.setFrom("xxx");
+        email.setSubject("hello");
+        email.setMsg(content);
+        email.addTo("xxx");
+        email.send();
+
+        return DataResponse.create()
+                ;
+    }
+
     private final RedisTemplate<String, String> template;
 
     private final UserService userService;
+
+    private final Configuration configuration;
 
 }
